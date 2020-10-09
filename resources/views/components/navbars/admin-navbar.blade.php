5576ed6f4aeeea6295e1a424647a67bd4ad2f23b
audrey-134957 <milard.audrey@gmail.com>
2020-10-09 22:51:19 +0200

    nettoyage code

--- resources/views/components/navbars/admin-navbar.blade.php
+++ resources/views/components/navbars/admin-navbar.blade.php
@@ -2,7 +2,7 @@
     <button class="admin-navbar__button admin-navbar__button--menu button is-success is-outlined is-rounded"><i class="admin-navbar__icon fa fa-bars" aria-hidden="true"></i></button>
 
     <figure class="admin-navbar__image-figure image is-70xauto">
-        <img class="admin-navbar__logo" src="{{asset('./images/logo/logo.png')}}" alt="logo du site web">
+        <img class="admin-navbar__logo" src="{{asset('./images/logo/logo-r.png')}}" alt="logo du site web">
     </figure>
 
     <a href="{{route('admin.logout', ['adminId' => auth()->user()->id])}}" class="admin-navbar__link button is-success is-outlined is-rounded"><i class="admin-navbar__icon fa fa-power-off" aria-hidden="true"></i><span class="admin-navbar__link-name">déconnexion</span></a>
